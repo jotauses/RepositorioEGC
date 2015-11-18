a23f533405dc0f9e0d4dd4034f3a73622b461339
jotauses <jota.us.es@gmail.com>
2015-11-18 19:58:46 +0100

    Añadido comentarios a cuadrado.py
    Añadido comentarios a cuadrado.py

--- cuadrado.py
+++ cuadrado.py
@@ -3,7 +3,9 @@ def __init__(self, x, y)
 	self.y=y
 	
 def area (self):
+"""porquería de linux"""
 	return self.x*self.y
 
 def perimeter(self, x, y):
+""" Esta función no vale pa na"""
 	return (2 * self.x) + (2 * self.y)
